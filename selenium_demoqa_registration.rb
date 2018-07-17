2aebe76249bd64b0517181028fff37ee3a80a30e
kenl94 <KLy@spartaglobal.com>
2018-07-17 08:01:28 +0100

    got the easy tests done

--- selenium_demoqa_registration.rb
+++ selenium_demoqa_registration.rb
@@ -6,21 +6,21 @@ class SeleniumDemoReg
   PAGE_URL = 'http://demoqa.com/registration/'
   # Page field
   FIRST_NAME_FIELD = "name_3_firstname" # id
-  LAST_NAME_FIELD =  # id
-  MARITAL_STATUS =  # id
-  HOBBY_STATUS =  # values
-  COUNTRY_DROP_DOWN_LIST =  # id
-  DOB_MONTH_DROPDOWN_LIST =  # id
-  DOB_DAY_DROPDOWN_LIST =  # id
-  DOB_YEAR_DROPDOWN_LIST =  # id
-  PHONE_NUMBER_FIELDS =  # id
-  USERNAME_FIELD =  # id
-  EMAIL_FIELD =  # id
-  PROFILE_PICTURE_BUTTON =  # id
-  DESCRIPTION_FIELD =  # id
-  PASSWORD_FIELD = # id
-  CONFIRM_PASSWORD_FIELD =  # id
-  SUBMIT_BUTTON = # name
+  LAST_NAME_FIELD = "name_3_lastname" # id
+  MARITAL_STATUS = "radio_fields" # id
+  HOBBY_STATUS =  "dance" || "reading" || "cricket"# values
+  COUNTRY_DROP_DOWN_LIST = "dropdown_7" # id
+  DOB_MONTH_DROPDOWN_LIST = "mm_date_8" # id
+  DOB_DAY_DROPDOWN_LIST = "dd_date_8" # id
+  DOB_YEAR_DROPDOWN_LIST = "yy_date_8" # id
+  PHONE_NUMBER_FIELDS = "phone_9" # id
+  USERNAME_FIELD = "username" # id
+  EMAIL_FIELD = "email_1" # id
+  PROFILE_PICTURE_BUTTON = "profile_pic_10" # id
+  DESCRIPTION_FIELD = "description" # id
+  PASSWORD_FIELD = "password_2" # id
+  CONFIRM_PASSWORD_FIELD = "confirm_password_password_2"# id
+  SUBMIT_BUTTON = "pie_submit" # name
   REGISTRATION_CONFIRMATION = #class
 
   def initialize
@@ -48,7 +48,7 @@ class SeleniumDemoReg
   end
 
   def first_name_field_displayed
-    @chrome_driver.find_element(:id, FIRST_NAME_FIELD)
+    @chrome_driver.find_element(:id, FIRST_NAME_FIELD).displayed?
   end
 
   # last name field management - Difficulty Easy
@@ -58,15 +58,27 @@ class SeleniumDemoReg
   end
 
   def get_last_name_field_value
+    @chrome_driver.find_element(:id, LAST_NAME_FIELD)["value"]
   end
 
   def last_name_field_displayed
+    @chrome_driver.find_element(:id, FIRST_NAME_FIELD).displayed?
   end
 
   # Marital option management - Difficulty Medium
 
   def select_marital_option(marital_status)
     # Consider something like a case statement and check the selenium selected? method
+    status = @chrome_driver.find_elements(:class, MARITAL_STATUS)
+    case status
+    when marital_status == "single"
+      radio_fields[0].click
+    when marital_status == "married"
+      radio_fields[1].click
+    when marital_status == "divorced"
+      radio_fields[2].click
+    end
+    sleep 5
   end
 
   # hobby option management - Difficulty Medium
@@ -83,6 +95,7 @@ class SeleniumDemoReg
   # If no solution then a run through will happen once finished
 
   def get_selected_country
+
   end
 
   def country_dropdown_list_select(country)
@@ -110,54 +123,72 @@ class SeleniumDemoReg
   # Phone number field management - Difficulty Easy
 
   def set_phone_number_field(phone_number)
+    @chrome_driver.find_element(:id, PHONE_NUMBER_FIELDS).send_keys(phone_number)
   end
 
   def get_phone_number_field_value
+    @chrome_driver.find_element(:id, PHONE_NUMBER_FIELDS)["value"]
   end
 
   #  username field management - Difficulty Easy
 
   def set_user_name_field(user_name)
+    @chrome_driver.find_element(:id, USERNAME_FIELD).send_keys(user_name)
   end
 
   def get_user_name_field_value
+    @chrome_driver.find_element(:id, USERNAME_FIELD)["value"]
   end
 
   # Email field management - Difficulty Easy
 
   def set_email_field(email)
+    @chrome_driver.find_element(:id, EMAIL_FIELD).send_keys(email)
   end
 
   def get_email_field_value
+    @chrome_driver.find_element(:id, EMAIL_FIELD)["value"]
   end
 
   # about yourself / description field - Difficulty Easy
 
   def set_about_yourself_field(details)
+    @chrome_driver.find_element(:id, DESCRIPTION_FIELD).send_keys(details)
   end
 
   def get_about_yourself_value
+    @chrome_driver.find_element(:id, DESCRIPTION_FIELD)["value"]
   end
 
   # Password management - Difficulty Easy
 
   def set_password_field(password)
+    @chrome_driver.find_element(:id, PASSWORD_FIELD).send_keys(password)
   end
 
   def get_password_value
+    @chrome_driver.find_element(:id, PASSWORD_FIELD)["value"]
   end
 
   def set_confirmation_password_field(password)
+    @chrome_driver.find_element(:id, CONFIRM_PASSWORD_FIELD).send_keys(password)
   end
 
   def get_confirmation_password_value
+    @chrome_driver.find_element(:id, CONFIRM_PASSWORD_FIELD)["value"]
   end
 
   # registration confirmation - Difficulty Easy
 
   def click_submit
+    @chrome_driver.find_element(:id, SUBMIT_BUTTON).click
   end
 
   def check_registration_successful
+
   end
 end
+
+runtest = SeleniumDemoReg.new
+runtest.access_registration_form
+runtest.select_marital_option("single")
--- spec/demoqa_registration_spec.rb
+++ spec/demoqa_registration_spec.rb
@@ -15,15 +15,18 @@ describe 'testing the demoqa registration page' do
     it 'should accept a first name' do
       @driver.set_first_name_field("Ken")
       expect(@driver.get_first_name_field_value).to eq "Ken"
+      expect(@driver.first_name_field_displayed)
     end
 
     it 'should accept a last name' do
-      @driver.set_first_name_field("Ly")
+      @driver.set_last_name_field("Ly")
       expect(@driver.get_last_name_field_value).to eq "Ly"
+      expect(@driver.last_name_field_displayed)
     end
 
     it 'should accept a marital status selection of Single, Married, or Divorced' do
-
+      # @driver.select_marital_option("single")
+      # expect(@driver.select_marital_option).to [0]
     end
 
     it 'should accept a hobby status selection of Dance, Reading, or Cricket' do
@@ -31,6 +34,8 @@ describe 'testing the demoqa registration page' do
     end
 
     it 'should have a country default of Afhghanistan' do
+
+
     end
 
     it 'accept a new DOB' do
@@ -42,23 +47,28 @@ describe 'testing the demoqa registration page' do
     end
 
     it 'should accept a valid phone number' do
-
+      @driver.set_phone_number_field("44075819723")
+      expect(@driver.get_phone_number_field_value).to eql "44075819723"
     end
 
     it 'should accept a username' do
-
+      @driver.set_user_name_field("USERNAME 1")
+      expect(@driver.get_user_name_field_value).to eql "USERNAME 1"
     end
 
     it 'should accept a about yourself text' do
-
+      @driver.set_about_yourself_field("LOREM IPSUM")
+      expect(@driver.get_about_yourself_value).to eql"LOREM IPSUM"
     end
 
     it 'should accept a password' do
-
+      @driver.set_password_field("PASSWORDQWERTY@1234")
+      expect(@driver.get_password_value).to eql "PASSWORDQWERTY@1234"
     end
 
     it 'should accept a password confirmation' do
-
+      @driver.set_confirmation_password_field("PASSWORDQWERTY@1234")
+      expect(@driver.get_confirmation_password_value).to eql "PASSWORDQWERTY@1234"
     end
 
   end
